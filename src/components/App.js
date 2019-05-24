88572d6f442bf695674c8a8ba4f7d77dc6eed48a
walterlee <walter.lee9@gmail.com>
2019-05-24 15:46:33 -0500

    intro component added

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Intro from './intro';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <Intro />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Intro() {
+    return (
+        <div>
+            <p style={{ fontSize: 50 }}>
+                React with Redux
+            </p>
+            <hr />
+        </div>
+    );
+}
+
+export default Intro;
