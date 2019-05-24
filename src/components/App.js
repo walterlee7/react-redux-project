740ca31f37f9eccf35319eda8d4111bc92119c10
walterlee <walter.lee9@gmail.com>
2019-05-24 17:59:06 -0500

    action types added

--- src/components/App.js
+++ src/components/App.js
@@ -5,43 +5,43 @@ import './App.css';
 
 import { createStore } from 'redux';
 
-const store = createStore(reducer);
+const initialState = {
+    count: 0
+};
+
+// function reducer(state = initialState, action) {
+//     console.log('reducer', state, action);
+//     return state;
+// }
+
+var store = createStore(reducer);
+
 store.dispatch({ type: "INCREMENT" });
 store.dispatch({ type: "INCREMENT" });
 store.dispatch({ type: "DECREMENT" });
 store.dispatch({ type: "RESET" });
 
-var initialState = {
-    count: 0
-};
-
-console.log(initialState)
-
 function reducer(state = initialState, action) {
+
     console.log('reducer', state, action);
-    return state;
+
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        default:
+            return state;
+    }
 }
-
-// function reducer(state = initialState, action) {
-//     console.log('reducer', state, action);
-
-//     switch (action.type) {
-//         case 'INCREMENT':
-//             return {
-//                 count: state.count + 1
-//             };
-//         case 'DECREMENT':
-//             return {
-//                 count: state.count - 1
-//             };
-//         case 'RESET':
-//             return {
-//                 count: 0
-//             };
-//         default:
-//             return state;
-//     }
-// }
 
 
 function App() {
