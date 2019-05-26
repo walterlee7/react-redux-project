ac4ccd1a59048494632521ab6756ed460130e441
walterlee <walter.lee9@gmail.com>
2019-05-26 17:23:10 -0500

    Updates

--- precache-manifest.8dcc1603ca855a2eb39342e0a5a6b1d7.js
+++ precache-manifest.8dcc1603ca855a2eb39342e0a5a6b1d7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0931b57440dde91d77a8fe9e7fbe2de0",
+    "revision": "f45c07f31b98e03cf65361792603f3fe",
     "url": "/react-redux-project/index.html"
   },
   {
-    "revision": "f52ae23007255ff33bb2",
+    "revision": "8e882813f6695eab0f6f",
     "url": "/react-redux-project/static/css/main.a30b7653.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-redux-project/static/js/2.d7fe3604.chunk.js"
   },
   {
-    "revision": "f52ae23007255ff33bb2",
-    "url": "/react-redux-project/static/js/main.8dfd6d4e.chunk.js"
+    "revision": "8e882813f6695eab0f6f",
+    "url": "/react-redux-project/static/js/main.cf08e7fa.chunk.js"
   },
   {
     "revision": "191dba23326c498b68f6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-redux-project/precache-manifest.c86648cba0ea9c9c3da1574c7f9e380c.js"
+  "/react-redux-project/precache-manifest.8dcc1603ca855a2eb39342e0a5a6b1d7.js"
 );
 
 self.addEventListener('message', (event) => {
