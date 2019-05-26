67107240f98a28dba6c90091fc982b052f35d823
walterlee <walter.lee9@gmail.com>
2019-05-26 17:07:20 -0500

    Updates

---
+++ precache-manifest.5cf01e681c36b0ecd92dc4bd6e434dcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b93c2aa699db5a377d77606a0bd26f",
+    "url": "/react-redux-project/index.html"
+  },
+  {
+    "revision": "c944053d263ace1c69ad",
+    "url": "/react-redux-project/static/css/main.a30b7653.chunk.css"
+  },
+  {
+    "revision": "885b49d79d450962a94f",
+    "url": "/react-redux-project/static/js/2.d7fe3604.chunk.js"
+  },
+  {
+    "revision": "c944053d263ace1c69ad",
+    "url": "/react-redux-project/static/js/main.15717103.chunk.js"
+  },
+  {
+    "revision": "191dba23326c498b68f6",
+    "url": "/react-redux-project/static/js/runtime~main.a5e63d4b.js"
+  }
+]);
